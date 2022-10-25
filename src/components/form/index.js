905c32d8391fa07dfbf7e63d4fa93d9ff9382863
Stephanie Hill <stephni.hill@gmail.com>
2022-10-24 18:15:55 -0700

    functional components

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 import './form.scss';
 
-class Form extends React.Component {
+const Form = (props) => {
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const formData = {
       method:'GET',
       url: 'https://pokeapi.co/api/v2/pokemon',
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData);
   }
 
-  render() {
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
@@ -31,7 +30,6 @@ class Form extends React.Component {
         </form>
       </>
     );
-  }
 }
 
 export default Form;
